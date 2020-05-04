3e7b9c22af3702e674453f236e95f054ce88cf7b
Twinkle gupta <gtwinkle132@gmail.com>
2020-05-04 18:43:30 +0530

    Create Quadratic_Equation_Roots.cpp

---
+++ Mathematics/Quadratic_Equation_Roots.cpp
@@ -0,0 +1,19 @@
+**rest of code is available in IDE**
+
+void quadraticRoots(int a,int b, int c)
+{
+    float D = b*b-4*a*c;
+    if(D<0)
+     cout<<"Imaginary";
+    else if(D==0)
+    {
+       float r= floor((-b + sqrt(D)) / (2.0*a));
+       cout<<r<<" "<<r;
+    }
+    else
+     {
+         float r1 = floor((-b+sqrt(D))/2.0*a);
+         float r2 = floor((-b-sqrt(D))/2.0*a);
+         cout<<r1<<" "<<r2;
+     }
+}

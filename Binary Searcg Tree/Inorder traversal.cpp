ae06f2d9621be2c3cb343386d15557c1cb23b440
Twinkle gupta <gtwinkle132@gmail.com>
2020-06-04 15:22:17 +0530

    Create Inorder traversal.cpp

---
+++ Binary Searcg Tree/Inorder traversal.cpp
@@ -0,0 +1,30 @@
+void inorder(, ,)
+    stack<Node *> s; 
+    Node *curr = root; 
+  
+    while (curr != NULL || s.empty() == false) 
+    { 
+        /* Reach the left most Node of the 
+           curr Node */
+        while (curr !=  NULL) 
+        { 
+            /* place pointer to a tree node on 
+               the stack before traversing 
+              the node's left subtree */
+            s.push(curr); 
+            curr = curr->left; 
+        } 
+  
+        /* Current must be NULL at this point */
+        curr = s.top(); 
+        s.pop(); 
+  
+        cout << curr->data << " "; 
+  
+        /* we have visited the node and its 
+           left subtree.  Now, it's right 
+           subtree's turn */
+        curr = curr->right; 
+  
+    } /* end of while */
+} 

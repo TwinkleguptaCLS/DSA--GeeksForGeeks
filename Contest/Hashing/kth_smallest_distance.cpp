cd2f7828a1d29fd49315c32d87381130012ee93f
Twinkle Gupta <gtwinkle132@gmail.com>
2021-07-06 11:23:31 +0530

    Create kth_smallest_distance.cpp

---
+++ Contest/Hashing/kth_smallest_distance.cpp
@@ -0,0 +1,30 @@
+int kthDiff(int a[], int n, int k) 
+{ 
+   //Your code here
+    sort(a,a+n);
+    vector<int>nums(a,a+n);
+    int l = nums[1] - nums[0];
+    for(int i = 2; i < n; i++){
+        if(nums[i] - nums[i-1] < l){
+            l = nums[i] - nums[i-1];
+        } 
+    }
+    int r = nums[n-1] - nums[0];
+    while(l<r){
+        int mid = l + (r - l)/2;
+        int count = 0;
+        for(int i = 0; i < n; i++){
+            int pos = upper_bound(nums.begin(), nums.end(), nums[i] + mid) - nums.begin();
+            count += pos - i - 1;
+        }
+        if(count < k){
+            l = mid+1;
+        }
+        else
+            r = mid;
+    }
+    return l;
+  
+  
+  
+} 

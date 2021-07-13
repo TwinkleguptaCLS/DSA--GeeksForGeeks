9619f7d237e929d7ede5f68fa15920be84012a8e
Akash Chandra <63443208+elit3H4cker@users.noreply.github.com>
2021-07-13 17:16:02 +0530

    int main() function is missing

--- Contest/Hashing/kth_smallest_distance.cpp
+++ Contest/Hashing/kth_smallest_distance.cpp
@@ -27,4 +27,17 @@ int kthDiff(int a[], int n, int k)
   
   
   
-} 
+}
+int main(){
+    int test;
+    cin>>test;
+    while(test--){
+        int n,k;
+        cin>>n>>k;
+        int arr[n];
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        cout<<kthDiff(arr,n,k)<<endl;
+    }
+}
